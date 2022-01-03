7cdf346e3ece7ce4839651d4a4fb062535963baf
Daniela Palumbo <daniela.palumbo@outlook.com>
2022-01-03 15:46:08 -0300

    first commit

---
+++ sueca/Card.swift
@@ -0,0 +1,8 @@
+//
+//  Card.swift
+//  sueca
+//
+//  Created by Daniela Palumbo on 15/09/21.
+//
+
+import Foundation
---
+++ sueca/EditCard.swift
@@ -0,0 +1,8 @@
+//
+//  EditCard.swift
+//  sueca
+//
+//  Created by Daniela Palumbo on 16/09/21.
+//
+
+import Foundation
